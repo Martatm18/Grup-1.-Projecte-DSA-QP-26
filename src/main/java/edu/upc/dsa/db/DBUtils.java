662b0d0274db962517aceda35cad9171343da377
carlalujan <carla.lujan@estudiantat.upc.edu>
2026-05-20 15:12:36 +0200

    dbutils actualizado

--- src/main/java/edu/upc/dsa/db/DBUtils.java
+++ src/main/java/edu/upc/dsa/db/DBUtils.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 public class DBUtils {
     public static final String DB_NAME = "protocolosigma";
     public static final String DB_HOST = "127.0.0.1";
-    public static final String DB_USER = "root";
+    public static final String DB_USER = "dsaUser";
     public static final String DB_PASS = "Mazinger72";
     public static final String DB_PORT = "3306";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
                 "jdbc:mariadb://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME +
-                "?user=" + DB_USER + "&password=" + DB_PASS
+                        "?user=" + DB_USER + "&password=" + DB_PASS
         );
     }
 }
